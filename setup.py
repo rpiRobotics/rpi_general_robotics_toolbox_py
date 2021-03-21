e4afec1db7d0488e764a290a387a9d980a05bdc4
wlawler45 <williamgmlawler@yahoo.com>
2021-03-21 19:34:34 -0400

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,11 +1,23 @@
-## ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
 
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
-# fetch values from package.xml
-setup_args = generate_distutils_setup(
-    packages=['general_robotics_toolbox'],
-    package_dir={'': 'src'})
-
-setup(**setup_args)
+setuptools.setup(
+    name="general_robotics_toolbox", # Replace with your own username
+    version="0.0.1",
+    author="John Wason",
+    author_email="wason@wasontech.com",
+    description="General robotics toolbox developed by RPI",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rpiRobotics/rpi_general_robotics_toolbox_py",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["numpy"],
+    python_requires='>=2.7',
+)
