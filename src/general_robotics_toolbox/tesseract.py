ae00af3d1a7e1731a237256d7348c6110c906fd8
John Wason <wason@wasontech.com>
2022-09-29 00:05:35 -0400

    work on tesseract support

--- src/general_robotics_toolbox/tesseract.py
+++ src/general_robotics_toolbox/tesseract.py
@@ -1,7 +1,7 @@
 import numpy as np
 from . import general_robotics_toolbox as rox
 
-from tesseract_robotics.tesseract_common import Translation3d, AngleAxisd, Isometry3d
+from tesseract_robotics.tesseract_common import Translation3d, AngleAxisd, Isometry3d, vector_pair_string
 from tesseract_robotics.tesseract_environment import Environment, Commands, \
     AddLinkCommand, AddKinematicsInformationCommand, AddSceneGraphCommand
 from tesseract_robotics.tesseract_scene_graph import Link, Joint, JointLimits, \
@@ -9,7 +9,9 @@ from tesseract_robotics.tesseract_scene_graph import Link, Joint, JointLimits, \
 from tesseract_robotics.tesseract_common import FilesystemPath, ManipulatorInfo, KinematicsPluginInfo, \
     PluginInfoContainer
 from tesseract_robotics.tesseract_kinematics import KinGroupIKInput, KinGroupIKInputs
-from tesseract_robotics.tesseract_srdf import KinematicsInformation
+from tesseract_robotics.tesseract_srdf import KinematicsInformation, parseKinematicsPluginConfigString
+import yaml
+import io
 
 def _transform_to_isometry3d(T):
     # TODO: Use better Isometry3d constructor
@@ -57,7 +59,10 @@ def _get_robot_world_to_base_joint(robot, robot_name):
     joint.child_link_name = f"{robot_name}_base_link"
     return joint
 
-def robot_to_scene_graph(robot, return_names = False, invkin_solver = ""):
+def _prefix_names(names, robot_name):
+    return [f"{robot_name}_{n}" for n in names]
+
+def robot_to_scene_graph(robot, return_names = False):
     sg = SceneGraph()
 
     sg_link_names = ["base_link"]
@@ -71,7 +76,7 @@ def robot_to_scene_graph(robot, return_names = False, invkin_solver = ""):
         joint_names = robot.joint_names
 
     # if robot.link_names is None:
-    link_names = [f"link_{i}" for i in range(n_joints+1)]
+    link_names = [f"link_{i+1}" for i in range(n_joints)]
     # else:
         # link_names = [_p(s) for s in robot.link_names]
 
@@ -89,11 +94,11 @@ def robot_to_scene_graph(robot, return_names = False, invkin_solver = ""):
             parent_link_name))
         sg_link_names.append(link_names[i])
     
-    assert sg.addLink(*_get_fixed_link_and_joint(rox.Transform(np.eye(3), robot.P[:,n_joints-1]), 
-        link_names[n_joints], link_names[n_joints] + "_chain_tip", link_names[n_joints-1]))
-    sg_link_names.append(link_names[n_joints])
+    assert sg.addLink(*_get_fixed_link_and_joint(rox.Transform(np.eye(3), robot.P[:,n_joints]), 
+        "chain_tip", link_names[n_joints-1] + "_to_chain_tip", link_names[n_joints-1]))
+    sg_link_names.append("chain_tip")
 
-    tip_link = link_names[n_joints]
+    tip_link = "chain_tip"
 
     flange_link_name = "flange"
 
@@ -116,7 +121,7 @@ def robot_to_scene_graph(robot, return_names = False, invkin_solver = ""):
     if not return_names:
         return sg
     else:
-        return sg, sg_link_names, joint_names
+        return sg, sg_link_names, joint_names, ["base_link"] + link_names
 
 def world_scene_graph(world_link_name = "world"):
     sg = SceneGraph()
@@ -124,7 +129,32 @@ def world_scene_graph(world_link_name = "world"):
     return sg
 
 
-def robot_to_tesseract_env_commands(robot, robot_name = "robot", include_world = True, return_names = False):
+def tesseract_kinematics_information(robot, robot_name, link_names, joint_names, chain_link_names, invkin_solver = None, 
+    invkin_plugin_info = None, base_link = None, tip_link = None):
+    
+    if base_link is None:
+        base_link = link_names[0]
+    if tip_link is None:
+        tip_link = link_names[-1]
+
+    kinematics_information = KinematicsInformation()
+    chain_group = vector_pair_string()
+    chain_group.append((base_link, tip_link))
+    kinematics_information.addChainGroup(robot_name, chain_group)
+    
+    plugin_info_str = kinematics_plugin_info_string(robot, robot_name, link_names, joint_names, chain_link_names,
+        invkin_solver, invkin_plugin_info, base_link, tip_link)
+
+    
+    plugin_info = parseKinematicsPluginConfigString(plugin_info_str)
+
+    kinematics_information.kinematics_plugin_info = plugin_info
+
+    return kinematics_information
+
+
+def robot_to_tesseract_env_commands(robot, robot_name = "robot", include_world = True, return_names = False, 
+    invkin_solver = None, invkin_plugin_info = None):
         
     commands = Commands()
     
@@ -132,20 +162,21 @@ def robot_to_tesseract_env_commands(robot, robot_name = "robot", include_world =
         world_sg = world_scene_graph()
         commands.append(AddSceneGraphCommand(world_sg))
 
-    robot_sg, sg_link_names, sg_joint_names = robot_to_scene_graph(robot, return_names = True)
+    robot_sg, sg_link_names, sg_joint_names, sg_chain_link_names = robot_to_scene_graph(robot, return_names = True)
     robot_base_joint = _get_robot_world_to_base_joint(robot, robot_name)
     commands.append(AddSceneGraphCommand(robot_sg, robot_base_joint, f"{robot_name}_"))
 
-    link_names = [f"{robot_name}_{s}" for s in sg_link_names]
-    joint_names = [f"{robot_name}_{s}" for s in sg_joint_names]
+    link_names = _prefix_names(sg_link_names, robot_name)
+    joint_names = _prefix_names(sg_joint_names, robot_name)
+    chain_link_names = _prefix_names(sg_chain_link_names, robot_name)
     
     # if robot.joint_names is None:
     #     joint_names = [f"{robot_name}_joint_{i+1}" for i in range(len(robot.joint_type))]
     # else:
     #     joint_names = [f"{robot_name}_{s}" for s in robot.joint_names]
     
-    kinematics_information = KinematicsInformation()
-    kinematics_information.addJointGroup(robot_name, joint_names)
+    kinematics_information = tesseract_kinematics_information(robot, robot_name, link_names, joint_names, 
+        chain_link_names, invkin_solver, invkin_plugin_info, "world", link_names[-1])
     commands.append(AddKinematicsInformationCommand(kinematics_information))
 
     if not return_names:
@@ -153,31 +184,103 @@ def robot_to_tesseract_env_commands(robot, robot_name = "robot", include_world =
     else:
         return commands, link_names, joint_names
 
-def kinematics_plugin_fwdkin_kdl_command(plugin_factory, robot_name, base_link, tip_link):
-    plugin_config = {
-        "kinematic_plugins": {
-            "search_libraries": [
-                "tesseract_kinematics_kdl_factories"
-            ],
-            "fwk_kin_plugins": {
-                robot_name: {
-                    "default": "KDLFwdKinChain",
-                    "plugins": {
-                        "KDLFwdKinChain": {
-                            "class": "KDLFwdKinChainFactory",
-                            "config": {
-                                "base_link": base_link,
-                                "tip_link": tip_link
-                            }
+def kinematics_plugin_fwdkin_kdl_plugin_info_dict(robot_name, base_link, tip_link):
+    plugin_info = {        
+        "fwd_kin_plugins": {
+            robot_name: {
+                "default": "KDLFwdKinChain",
+                "plugins": {
+                    "KDLFwdKinChain": {
+                        "class": "KDLFwdKinChainFactory",
+                        "config": {
+                            "base_link": base_link,
+                            "tip_link": tip_link
                         }
                     }
                 }
             }
         }
     }
+    
+    return plugin_info, ["tesseract_kinematics_kdl_factories"]
 
-    PluginInfoContainer()
+def kinematics_plugin_invkin_kdl_plugin_info_dict(robot_name, base_link, tip_link, default_solver = "KDLInvKinChainLMA"):
+    plugin_info = {
+        "inv_kin_plugins": {
+            robot_name: {
+                "default": default_solver,
+                "plugins": {
+                    "KDLInvKinChainLMA": {
+                        "class": "KDLInvKinChainLMAFactory",
+                        "config": {
+                            "base_link": base_link,
+                            "tip_link": tip_link
+                        }
+                    },
+                    "KDLInvKinChainNR": {
+                        "class": "KDLInvKinChainNRFactory",
+                        "config": {
+                            "base_link": base_link,
+                            "tip_link": tip_link
+                        }
+                    }
+                }
+            }
+        }        
+    }
 
-    plugin_info = KinematicsPluginInfo()
+    return plugin_info, ["tesseract_kinematics_kdl_factories"]
 
- 
+def kinematics_plugin_info_dict(robot, robot_name, link_names, joint_names, chain_link_names, invkin_solver = None, 
+    invkin_plugin_info = None, base_link = None, tip_link = None):
+    
+    if base_link is None:
+        base_link = link_names[0]
+
+    if tip_link is None:
+        tip_link = link_names[-1]
+
+    # fwdkin_solver = "KDLFwdKinChain"
+    fwdkin_plugin_info, fwdkin_libs = kinematics_plugin_fwdkin_kdl_plugin_info_dict(
+        robot_name, base_link, tip_link)
+
+    if invkin_solver is None:
+        invkin_solver = "KDLInvKinChainLMA"
+
+    if invkin_plugin_info is not None:
+        invkin_plugin_info, invkin_libs = invkin_plugin_info
+    else:
+
+        if invkin_solver == "KDLInvKinChainLMA" or invkin_solver == "KDLInvKinChainNR":
+            invkin_plugin_info, invkin_libs = kinematics_plugin_invkin_kdl_plugin_info_dict(robot_name,
+                base_link, tip_link, invkin_solver)
+        elif invkin_solver == "OPWInvKin":
+            assert False, "OPWInvKin not implemented"
+        elif invkin_solver == "URInvKin":
+            assert False, "URInvKin not implemented"
+        else:
+            assert False, "Unknown inverse kinematics solver"
+
+    libs = set(fwdkin_libs + invkin_libs)
+
+    plugin_info_dict = {
+        "kinematic_plugins": {
+            "search_libraries": list(libs),        
+            "fwd_kin_plugins": fwdkin_plugin_info["fwd_kin_plugins"],
+            "inv_kin_plugins": invkin_plugin_info["inv_kin_plugins"],
+        }
+    }
+
+    return plugin_info_dict
+        
+def kinematics_plugin_info_string(robot, robot_name, link_names, joint_names, chain_link_names, invkin_solver = None, 
+    invkin_plugin_info = None, base_link = None, tip_link = None):
+
+    plugin_info_dict = kinematics_plugin_info_dict(robot, robot_name, link_names, joint_names, chain_link_names,
+        invkin_solver, invkin_plugin_info, base_link, tip_link)
+
+    f = io.StringIO()
+    yaml.safe_dump(plugin_info_dict, f)
+    plugin_info_str = f.getvalue()
+
+    return plugin_info_str
--- test/test_tesseract_kinematics.py
+++ test/test_tesseract_kinematics.py
@@ -18,6 +18,8 @@ from general_robotics_toolbox import robotraconteur as rr_rox
 from tesseract_robotics.tesseract_scene_graph import \
     JointType_FIXED, JointType_REVOLUTE, JointType_PRISMATIC
 from tesseract_robotics.tesseract_environment import Environment
+from tesseract_robotics.tesseract_common import TransformMap, Isometry3d
+from tesseract_robotics.tesseract_kinematics import KinGroupIKInput, KinGroupIKInputs
 
 def _assert_rox_eig_transform_close(T, eig_iso):
     H_eig_iso = eig_iso.matrix()
@@ -70,14 +72,124 @@ def _get_absolute_path(fname):
     dirname = os.path.dirname(os.path.realpath(__file__))
     return dirname + "/" + fname
 
-def test_robot_to_tesseract_env_commands():
+def _assert_sg_joint(sg_joint, name, parent, child, sg_type, h, p, l, u, v, a):
+    assert sg_joint.getName() == name
+    assert sg_joint.parent_link_name == parent
+    assert sg_joint.child_link_name == child
+    assert sg_joint.type == sg_type
+    sg_limits = sg_joint.limits
+    assert sg_limits.lower == l
+    assert sg_limits.upper == u
+    assert sg_limits.velocity == v
+    assert sg_limits.acceleration == a
+
+    _assert_rox_eig_transform_close(rox.Transform(np.eye(3),p), 
+        sg_joint.parent_to_joint_origin_transform)
+    nptest.assert_allclose(h, sg_joint.axis.flatten(), atol=1e-5)
+    
+def _assert_sg_fixed_joint(sg_joint, name, parent, child, q, p):
+    assert sg_joint.getName() == name
+    assert sg_joint.parent_link_name == parent
+    assert sg_joint.child_link_name == child
+    assert sg_joint.type == JointType_FIXED
+    T = rox.Transform(rox.q2R(q),p)
+    _assert_rox_eig_transform_close(T, sg_joint.parent_to_joint_origin_transform)
+
+def test_robot_to_scene_graph_sawyer():
+    with open(_get_absolute_path("sawyer_robot_with_electric_gripper_config.yml"), "r") as f:
+        robot = rr_rox.load_robot_info_yaml_to_robot(f)
+
+    sg, link_names, joint_names, chain_link_names = rox_tesseract.robot_to_scene_graph(robot, True)
+
+    assert link_names == ['base_link', 'link_1', 'link_2', 'link_3', 'link_4', 'link_5', 'link_6', 'link_7',
+     'chain_tip', 'flange', 'tool_tcp']
+         
+    assert joint_names ==  ['right_j0', 'right_j1', 'right_j2', 'right_j3', 'right_j4', 'right_j5', 'right_j6']
+    assert chain_link_names == ['base_link', 'link_1', 'link_2', 'link_3', 'link_4', 'link_5', 'link_6', 'link_7']
+
+    sg_links_v = sg.getLinks()
+    sg_joints_v = sg.getJoints()
+    sg_links = {sg_links_v[i].getName(): sg_links_v[i] for i in range(len(sg_links_v))}
+    sg_joints = {sg_joints_v[i].getName(): sg_joints_v[i] for i in range(len(sg_joints_v))}
+
+    assert set(sg_links.keys()) == set(['link_1', 'base_link', 'link_2', 'link_3', 'link_4', 'link_5', 'link_6', 
+        'link_7', 'chain_tip', 'flange', 'tool_tcp'])
+    assert set(sg_joints.keys()) == set(['right_j0', 'right_j1', 'right_j2', 'link_7_to_chain_tip',
+         'right_j3', 'right_j4', 'right_j5', 'right_j6', 'tip_to_flange', 'tip_to_tool'])
+
+    _assert_sg_joint(sg_joints["right_j0"], "right_j0", "base_link", "link_1", JointType_REVOLUTE, 
+    [0,0,1], [0,0,0.08], -3.0503, 3.0503, 1.74, 3.5)
+    _assert_sg_joint(sg_joints["right_j1"], "right_j1", "link_1", "link_2", JointType_REVOLUTE, 
+    [0,1,0], [0.081,0.05,0.237], -3.8095, 2.2736, 1.328, 2.5)
+    _assert_sg_joint(sg_joints["right_j2"], "right_j2", "link_2", "link_3", JointType_REVOLUTE, 
+    [1,0,0], [0.14,0.1425,0], -3.0426, 3.0426, 1.957, 5.0)
+    _assert_sg_joint(sg_joints["right_j3"], "right_j3", "link_3", "link_4", JointType_REVOLUTE, 
+    [0,1,0], [0.26,-0.042,0], -3.0439, 3.0439, 1.957, 5.0)
+    _assert_sg_joint(sg_joints["right_j4"], "right_j4", "link_4", "link_5", JointType_REVOLUTE, 
+    [1,0,0], [0.125,-0.1265,0], -2.9761, 2.9761, 3.485, 5.0)
+    _assert_sg_joint(sg_joints["right_j5"], "right_j5", "link_5", "link_6", JointType_REVOLUTE, 
+    [0,1,0], [0.275,0.031,0], -2.9761, 2.9761, 3.485, 5.0)
+    _assert_sg_joint(sg_joints["right_j6"], "right_j6", "link_6", "link_7", JointType_REVOLUTE, 
+    [1,0,0], [0.11,0.1053,0], -4.7124, 4.7124, 4.545, 5.0)
+    _assert_sg_fixed_joint(sg_joints["link_7_to_chain_tip"], "link_7_to_chain_tip", "link_7", "chain_tip", \
+        [1,0,0,0], [0.0245,0,0])
+    _assert_sg_fixed_joint(sg_joints["tip_to_flange"], "tip_to_flange", "chain_tip", "flange", \
+        [-0.454518, 0.541676, -0.454521, 0.541672], [0,0,0])
+    _assert_sg_fixed_joint(sg_joints["tip_to_tool"], "tip_to_tool", "flange", "tool_tcp", \
+        [0.999048, 0, 0, 0.043619], [0,0,0.1577])
+
+def test_robot_to_tesseract_env_commands_and_kin_sawyer():
     with open(_get_absolute_path("sawyer_robot_with_electric_gripper_config.yml"), "r") as f:
         robot = rr_rox.load_robot_info_yaml_to_robot(f)
 
     tesseract_env_commands = rox_tesseract.robot_to_tesseract_env_commands(robot, "sawyer")
     env = Environment()
-    env.init(tesseract_env_commands)
+    assert env.init(tesseract_env_commands)
 
-    kin_info = env.getKinematicsInformation()
+    # kin_info = env.getKinematicsInformation()
+    kin_group = env.getKinematicGroup("sawyer")
+    kin_group_joint_names = kin_group.getJointNames()
+    kin_group_link_names = kin_group.getLinkNames()
 
-    print(kin_info)
+
+    assert set(kin_group_joint_names) == set(['sawyer_right_j0', 'sawyer_right_j1', 'sawyer_right_j2', 'sawyer_right_j3', 
+        'sawyer_right_j4', 'sawyer_right_j5', 'sawyer_right_j6'])
+
+    assert set(kin_group_link_names) == set(['sawyer_link_5', 'sawyer_base_link', 'world', 'sawyer_link_1', 
+        'sawyer_link_0', 'sawyer_link_2', 'sawyer_link_3', 'sawyer_link_4', 'sawyer_link_6', 'sawyer_link_7',
+        'sawyer_flange', 'sawyer_tool_tcp'])
+
+    q = np.ones((7,),dtype=np.float64)*np.deg2rad(15)
+    tcp_pose = kin_group.calcFwdKin(q)["sawyer_tool_tcp"]
+    
+    ik = KinGroupIKInput()
+    ik.pose = tcp_pose
+    ik.tip_link_name = "sawyer_tool_tcp"
+    ik.working_frame = "world"
+    iks = KinGroupIKInputs()
+    iks.append(ik)
+
+    invkin1 = kin_group.calcInvKin(iks,q*0.7)
+
+    nptest.assert_allclose(q, invkin1[0].flatten(), atol=np.deg2rad(0.5))
+
+
+def test_kinematics_plugin_info_string_sawyer():
+    with open(_get_absolute_path("sawyer_robot_with_electric_gripper_config.yml"), "r") as f:
+        robot = rr_rox.load_robot_info_yaml_to_robot(f)
+
+    sg, link_names, joint_names, chain_link_names = rox_tesseract.robot_to_scene_graph(robot,True)
+
+    kin_plugin_info = rox_tesseract.kinematics_plugin_info_string(robot, "my_robot", link_names, joint_names, chain_link_names,
+        "KDLInvKinChainNR")
+
+    kin_plugin_info_expected = 'kinematic_plugins:\n  fwd_kin_plugins:\n    my_robot:\n      default: KDLFwdKinChain\n' \
+    '      plugins:\n        KDLFwdKinChain:\n          class: KDLFwdKinChainFactory\n          config:\n' \
+    '            base_link: base_link\n            tip_link: tool_tcp\n  inv_kin_plugins:\n    my_robot:\n' \
+    '      default: KDLInvKinChainNR\n      plugins:\n        KDLInvKinChainLMA:\n' \
+    '          class: KDLInvKinChainLMAFactory\n          config:\n            base_link: link_0\n' \
+    '            tip_link: link_7\n        KDLInvKinChainNR:\n          class: KDLInvKinChainNRFactory\n' \
+    '          config:\n            base_link: link_0\n            tip_link: link_7\n  search_libraries:\n' \
+    '  - tesseract_kinematics_kdl_factories\n'
+
+    assert kin_plugin_info == kin_plugin_info_expected
