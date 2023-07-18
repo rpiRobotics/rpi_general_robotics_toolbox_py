d0f94aaafa20dfdad2180f13f16089ad8bb74c31
John Wason <wason@wasontech.com>
2023-07-18 19:17:43 -0400

    Add slerp function (#22)

--- src/general_robotics_toolbox/__init__.py
+++ src/general_robotics_toolbox/__init__.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import
 from .general_robotics_toolbox import hat, invhat, rot, R2rot, screw_matrix, q2R, R2q, q2rot, rot2q, quatcomplement, \
     quatproduct, quatjacobian, rpy2R, R2rpy, Robot, Transform, fwdkin, robotjacobian, subproblem0, subproblem1, \
     subproblem2, subproblem3, subproblem4, apply_robot_aux_transforms, unapply_robot_aux_transforms, \
-    identity_transform, random_R, random_p, random_transform
+    identity_transform, random_R, random_p, random_transform, slerp
 
 from .general_robotics_toolbox_invkin import robot6_sphericalwrist_invkin, ur_invkin, equivalent_configurations, \
     iterative_invkin
@@ -12,5 +12,5 @@ __all__ = ['hat', 'invhat', 'rot', 'R2rot', 'screw_matrix', 'q2R', 'R2q', 'q2rot
     'quatproduct', 'quatjacobian', 'rpy2R', 'R2rpy', 'Robot', 'Transform', 'fwdkin', 'robotjacobian', 'subproblem0',
     'subproblem1', 'subproblem2', 'subproblem3', 'subproblem4', 'apply_robot_aux_transforms', 
     'unapply_robot_aux_transforms', 'identity_transform', 'random_R', 'random_p', 'random_transform', 
-    'robot6_sphericalwrist_invkin', 'ur_invkin', 'equivalent_configurations', 'iterative_invkin']
+    'robot6_sphericalwrist_invkin', 'ur_invkin', 'equivalent_configurations', 'iterative_invkin', 'slerp']
 
